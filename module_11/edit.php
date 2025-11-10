b1b7f4b9f0f8e8157d4d76abc1a84c7d13157bb6
InsideOutzz <don.qerimi@mileniumi3.net>
2025-11-10 20:48:44 +0100

    a

---
+++ module_11/edit.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once('config.php');
+
+
+$id = $_GET['id'];
+
+
+$sql = "SELECT * FROM users WHERE id = :id";
+
+
+$prep = $conn->prepare($sql);
+
+
+$prep->bindParam(':id', $id)
+
+
+$prep->execute();
+
+
+header('Location:dashboard.php');
+?>
